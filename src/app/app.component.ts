4c083fb44dda218110f66119b428b24cc88dee4b
Blake Bourque <techplex.engineer@gmail.com>
2016-08-10 22:39:53 -0400

    Heroes Quick Start using webpack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
-import '../../public/css/styles.css';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+      <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
+  directives: [HeroesComponent, ROUTER_DIRECTIVES],
+  precompile: [HeroesComponent, HeroDetailComponent, DashboardComponent],
+  providers: [
+    HeroService
+  ]
 })
-
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
